a03f30ccf59239932f1ccacf63fe2fd92fb41f4c
Armando Vasquez <mando@Armandos-MacBook-Pro.local>
2021-07-23 01:26:53 -0500

    Initial commit

---
+++ schemas/documents/article.tsx
@@ -0,0 +1,131 @@
+import React from 'react'
+import { FaPaperclip } from 'react-icons/fa'
+
+const highlightIcon = () => (
+  <span style={{ fontWeight: `bold` }}>H</span>
+)
+const highlightRender = props => (
+  <span style={{ backgroundColor: `yellow` }}>{props.children}</span>
+)
+const codeRender = props => (
+  <pre style={{
+    backgroundColor: `lightgray`
+  }}><code>{props.children}</code></pre>
+)
+
+export default {
+    name: `article`,
+    title: `Article`,
+    type: `document`,
+    fields: [
+      {
+        name: `title`,
+        title: `Title`,
+        type: `string`,
+        required: true,
+      },
+      {
+        name: `slug`,
+        title: `Slug`,
+        type: `slug`,
+        options: {
+          source: `title`,
+          slugify: (input: string) => input
+                           .toLowerCase()
+                           .replace(/\s+/g, `-`)
+                           .slice(0, 200)
+        },
+      },
+      {
+        title: `Publish date`,
+        name: `publishDate`,
+        type: `date`,
+        options: {
+          dateFormat: `YYYY-MM-DD`,
+          calendarTodayLabel: `Today`
+        },
+        required: true,
+      },
+      {
+        name: `image`,
+        title: `Image`,
+        type: `image`,
+        options: {
+          hotspot: true,
+        },
+      },
+      {
+        name: `body`,
+        title: `Body`,
+        type: `array`,
+        required: true,
+        of: [
+          {
+            title: `Block`,
+            type: `block`,
+            styles: [
+                { title: `Normal`, value: `normal` },
+                {title: `H1`, value: `h1`},
+                {title: `H2`, value: `h2`},
+                {title: `CodeBlock`, value: `pre`, blockEditor: {
+                  render: codeRender
+                }}
+            ],
+            marks: {
+                decorators: [
+                  { title: `Strong`, value: `strong` },
+                  { title: `Emphasis`, value: `em` },
+                  { title: `Code`, value: `code` },
+                  { title: `Highlight`, value: `highlight`, 
+                    blockEditor: {
+                      icon: highlightIcon,
+                      render: highlightRender
+                    }
+                  }
+                ],
+                annotations: [
+                  {
+                    name: `link`,
+                    type: `object`,
+                    title: `link`,
+                    fields: [
+                      {
+                        name: `url`,
+                        type: `url`
+                      }
+                    ]
+                  },
+                  {
+                    name: `internalLink`,
+                    type: `object`,
+                    title: `Internal link`,
+                    blockEditor: {
+                      icon: FaPaperclip
+                    },
+                    fields: [
+                      {
+                        name: `reference`,
+                        type: `reference`,
+                        to: [
+                          { type: `article` }
+                        ]
+                      }
+                    ]
+                  }
+                ],
+            },
+            lists: [],
+          },
+          {
+            type: `code`
+          }
+        ],
+      }
+    ],
+    preview: {
+      select: {
+        title: `name`,
+        media: `image`,
+      },
+    },
+  }
